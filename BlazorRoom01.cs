59254ec18bb59be00460a45213c29351c92373f7
kaninou <81600385+kaninou@users.noreply.github.com>
2021-04-02 21:25:32 +0200

    Create BlazorRoom01.cs

---
+++ BlazorRoom01.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+    }
+}
